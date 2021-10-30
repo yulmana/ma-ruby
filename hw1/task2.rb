a082c586dc1925a155a85743ee4e0a71f3f14a08
Yuliia Manastyrna <44682233+yulmana@users.noreply.github.com>
2021-10-30 13:29:54 +0000

    homework1

---
+++ hw1/task2.rb
@@ -0,0 +1,3 @@
+puts 'enter your age'
+age = gets.to_i
+puts "You will be 100 in #{100-age}"
---
+++ hw1/task3.rb
@@ -0,0 +1,3 @@
+puts 'enter your hobbies'
+hobbies = gets.split
+puts "Tell me something about #{hobbies[rand]}"
