0f82b0f26695f83c861d6f3b7fc40cc1f3646102
Yuliia Manastyrna <yulmana@gmail.com>
2021-11-14 16:43:17 +0200

    homework2edited

--- hw1/HW2/task1.rb
+++ hw1/HW2/task1.rb
@@ -1,7 +1,8 @@
-animals = {"dog" => "woof", "cat" => "meow", "cow" => "moo", "pig" => "oink"}
+animals = {dog: "woof", cat: "meow", cow: "moo", pig: "oink"}
 puts('What animal do you like?')
-newanimal = gets.chomp
-if animals.include?(newanimal.downcase)
-  puts("#{newanimal.capitalize} says #{animals[newanimal.downcase]}")
-else puts("Sorry. I don't know this animal")
+animal_name = gets.chomp
+if animals[animal_name.to_sym]
+  puts("#{animal_name.capitalize} says #{animals[animal_name.to_sym]}")
+else 
+  puts("Sorry. I don't know this animal")
 end
--- hw1/HW2/task2.rb
+++ hw1/HW2/task2.rb
@@ -1,11 +1,9 @@
 loop do
   i=0
   puts "Enter a number"
-  n = gets.chomp
-  array = []
-  n.each_char {|c| array << c.to_i }
-  array.each {|c| if c == 1; i=i+1 end}
+  number = gets.chomp
+  i = number.count('1')
   break if i>=2 
-  puts "Result is: #{256*n.to_i}"
+  puts "Result is: #{256*number.to_i}"
 end
 puts "Conditions are not met, I stop the program"
